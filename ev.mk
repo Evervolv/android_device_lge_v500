f25da2430fd3c172230abf056e346b6c1dc6620e
Drgravy <drg113001@gmail.com>
2014-05-06 06:12:39 -0500

    update ev.mk

--- ev.mk
+++ ev.mk
@@ -1,7 +1,14 @@
+# Get the long list of APNs
+PRODUCT_COPY_FILES := vendor/ev/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
 # Inherit device configuration
 $(call inherit-product, device/lge/v500/v500.mk)
+$(call inherit-product-if-exists, vendor/lge/v500/v500-vendor.mk)
 
-# Inherit AICP common_tablet bits
+# Inherit some common evervolv stuff.
 $(call inherit-product, vendor/ev/config/common_full_tablet_wifionly.mk)
 
 ## Device identifier. This must come after all inclusions
